f2894c3d228c6aeaa6d00cbf174611f815e357c2
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-05-03 00:05:14 +0300

    ssl

--- modules/load_balancer/regional.tf
+++ modules/load_balancer/regional.tf
@@ -31,7 +31,7 @@ resource "google_compute_target_pool" "cf-pks-harbor" {
 resource "google_compute_forwarding_rule" "cf-pks-harbor" {
   name        = "${var.env_name}-cf-pks-harbor"
   target      = "${google_compute_target_pool.cf-pks-harbor.self_link}"
-  ssl_certificate = "${module.harbor.ssl_certificate}"
+
   port_range  = "443"
   ip_protocol = "TCP"
   ip_address  = "${google_compute_address.cf-pks-harbor.address}"
