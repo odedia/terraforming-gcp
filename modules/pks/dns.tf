e84f3266e30c5e7fcbf18fe748bb5db81f071742
odedia <odedia@gmail.com>
2019-05-02 17:24:30 +0300

    Update dns.tf

--- modules/pks/dns.tf
+++ modules/pks/dns.tf
@@ -7,3 +7,14 @@ resource "google_dns_record_set" "wildcard-pks-dns" {
 
   rrdatas = ["${module.api.address}"]
 }
+
+resource "google_dns_record_set" "wildcard-pks-dns-harbor" {
+  name = "harbor.${var.dns_zone_dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${var.dns_zone_name}"
+
+  rrdatas = ["${google_compute_address.cf-pks-harbor.address}"]
+}
+
