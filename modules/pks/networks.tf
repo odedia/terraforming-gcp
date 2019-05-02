42c6cfa484cb04780a91b26377b4613951aff12f
odedia <PpE7^%&t5yCAAx4TA9EhDbqbZPeybn66>
2019-05-02 23:43:56 +0300

    ssl

--- modules/pks/networks.tf
+++ modules/pks/networks.tf
@@ -32,20 +32,20 @@ resource "google_compute_address" "cf-pks-harbor" {
 }
 
 // TCP target pool
-//resource "google_compute_target_pool" "cf-pks-harbor" {
-//  name = "${var.env_name}-cf-pks-harbor"
-  //instances = [ ] // leave commented out - not just empty - BOSH will manage thru the tile
-//}
+resource "google_compute_target_pool" "cf-pks-harbor" {
+  name = "${var.env_name}-cf-pks-harbor"
+  instances = [ ] // leave commented out - not just empty - BOSH will manage thru the tile
+}
 
 // TCP forwarding rule
-//resource "google_compute_forwarding_rule" "cf-pks-harbor" {
-//  name        = "${var.env_name}-cf-pks-harbor"
-//  target      = "${google_compute_target_pool.cf-pks-harbor.self_link}"
+resource "google_compute_forwarding_rule" "cf-pks-harbor" {
+  name        = "${var.env_name}-cf-pks-harbor"
+  target      = "${google_compute_target_pool.cf-pks-harbor.self_link}"
 
-//  port_range  = "443"
-//  ip_protocol = "TCP"
-//  ip_address  = "${google_compute_address.cf-pks-harbor.address}"
-//}
+  port_range  = "443"
+  ip_protocol = "TCP"
+  ip_address  = "${google_compute_address.cf-pks-harbor.address}"
+}
 
 resource "google_dns_record_set" "wildcard-pks-dns-harbor" {
   name = "harbor.pks.${var.dns_zone_dns_name}"
