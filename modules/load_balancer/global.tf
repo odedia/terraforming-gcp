b65778b306869ece37cbc99dbea66d575f9cf4df
odedia <odedia@gmail.com>
2019-05-02 17:22:55 +0300

    Update global.tf

--- modules/load_balancer/global.tf
+++ modules/load_balancer/global.tf
@@ -122,14 +122,4 @@ resource "google_compute_forwarding_rule" "cf-pks-harbor" {
   ip_address  = "${google_compute_address.cf-pks-harbor.address}"
 }
 
-resource "google_dns_record_set" "wildcard-pks-dns-harbor" {
-  name = "harbor.${google_dns_managed_zone.env_dns_zone_pks.dns_name}"
-  type = "A"
-  ttl  = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone_pks.name}"
-
-  rrdatas = ["${google_compute_address.cf-pks-harbor.address}"]
-}
-
   
